eb3c38e6a271085f3730b58193ba66f2ecd81d6a
dave@solidhouse.com <dave@solidhouse.com@b5b6f76c-248a-11de-9353-0b00b05e739f>
2009-04-09 15:16:39 +0000

    -Adding fluent Run and Copy

--- Build/MainBuildTask.cs
+++ Build/MainBuildTask.cs
@@ -34,7 +34,7 @@ namespace Build
         {
             var tools = new FileSet().Include(directory_tools.RecurseAllSubFolders().FileName("nunit.framework.dll"))
                                     .Include(directory_tools.RecurseAllSubFolders().FileName("rhino.mocks.dll"));
-            DirectoryUtility.CopyAllFiles(tools, directory_compile);
+            Copy.From(tools).To(directory_compile);
 
             FileSet sourceFiles = new FileSet().Include(directory_base.SubFolder("src").RecurseAllSubFolders().FileName("*.cs"));
             FluentBuild.Build build = CreateBuildTask.UsingCsc.AddSources(sourceFiles).AddRefences(thirdparty_rhino, thirdparty_nunit).OutputFileTo(assembly_FluentBuild);
@@ -46,14 +46,14 @@ namespace Build
             var tools = new FileSet().Include(directory_tools.RecurseAllSubFolders().FileName("nunit.framework.dll"))
                                      .Include(directory_tools.RecurseAllSubFolders().FileName("rhino.mocks.dll"));
 
-            DirectoryUtility.CopyAllFiles(tools, directory_compile);
+            Copy.From(tools).To(directory_compile);
             FileSet sourceFiles = new FileSet().Include(directory_base.SubFolder("tests").RecurseAllSubFolders().FileName("*.cs"));
             CreateBuildTask.UsingCsc.AddSources(sourceFiles).AddRefences(thirdparty_rhino, thirdparty_nunit, assembly_FluentBuild).OutputFileTo(assembly_FluentBuild_Tests).Execute();
         }
 
         private void RunTests()
         {
-            ProcessUtility.StartProcess(directory_tools.SubFolder("nunit").FileName("nunit-console.exe"), assembly_FluentBuild);
+            Run.Executeable(directory_tools.SubFolder("nunit").FileName("nunit-console.exe")).WithArguments(assembly_FluentBuild).Execute();
         }
 
       
