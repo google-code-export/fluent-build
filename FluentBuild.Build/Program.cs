a37ae953dc93861d5adc53933ad707c9941accd4
dave@solidhouse.com <dave@solidhouse.com@b5b6f76c-248a-11de-9353-0b00b05e739f>
2009-04-09 16:12:37 +0000

    -moving assembly info

--- FluentBuild.Build/Program.cs
+++ FluentBuild.Build/Program.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Build
+﻿namespace Build
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             var build = new MainBuildTask();
             build.Execute();
