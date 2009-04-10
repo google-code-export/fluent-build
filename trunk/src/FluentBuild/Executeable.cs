240f5d40fef8ea73fcf5f09995a5addf73570b75
dave@solidhouse.com <dave@solidhouse.com@b5b6f76c-248a-11de-9353-0b00b05e739f>
2009-04-10 21:28:31 +0000

    -Refactoring color support to add indentation and seperate the concerns a bit

--- trunk/src/FluentBuild/Executeable.cs
+++ trunk/src/FluentBuild/Executeable.cs
@@ -44,55 +44,58 @@ namespace FluentBuild
             var startInfo = new ProcessStartInfo(_executeablePath);
             startInfo.UseShellExecute = false;
             startInfo.Arguments = CreateArgumentString();
-
+            //redirect to a stream so we can parse it and display it
             startInfo.RedirectStandardOutput = true;
             startInfo.RedirectStandardError = true;
-
             if (!String.IsNullOrEmpty(_workingDirectory))
                 startInfo.WorkingDirectory = _workingDirectory;
 
             Process process = Process.Start(startInfo);
             if (process == null)
                 throw new ApplicationException("process did not start?");
+        
 
+            process.WaitForExit();
+            Environment.ExitCode += process.ExitCode;
+           
+            DisplayOutput(process, prefix);
+        }
+
+        private void DisplayOutput(Process process, string prefix)
+        {
+            ConsoleColor.BuildColor textColor = ConsoleColor.BuildColor.Default;
+           
             using (StreamReader output = process.StandardOutput)
-                using (StreamReader error = process.StandardError)
+            using (StreamReader error = process.StandardError)
+            {
+                if (process.ExitCode != 0)
+                    textColor = ConsoleColor.BuildColor.Red;
+
+                foreach (string line in output.ReadToEnd().Split(Environment.NewLine.ToCharArray()))
                 {
-                    process.WaitForExit();
-                    Environment.ExitCode += process.ExitCode;
-                    var textColor = ConsoleColor.BuildColor.Default;
-                    
-                    if (process.ExitCode != 0)
+                    if (line.Trim().Length > 0)
                     {
-                      textColor = ConsoleColor.BuildColor.Red;
+                        ConsoleColor.SetColor(textColor);
+                        if (line.Contains("warning") || line.Contains("Warning"))
+                            ConsoleColor.SetColor(ConsoleColor.BuildColor.Yellow);
+                        MessageLogger.Write(prefix, line);
                     }
-
-                    foreach (var line in output.ReadToEnd().Split(Environment.NewLine.ToCharArray()))
-                    {
-                        if (line.Trim().Length > 0)
-                        {
-                            ConsoleColor.SetColor(textColor);
-                            if (line.Contains("warning") || line.Contains("Warning"))
-                                ConsoleColor.SetColor(ConsoleColor.BuildColor.Yellow);
-                            MessageLogger.Write(prefix, line);
-                        }
-                    }
-
-                    ConsoleColor.SetColor(textColor);
-                    foreach (var line in error.ReadToEnd().Split(Environment.NewLine.ToCharArray()))
-                    {
-                        if (line.Trim().Length > 0)
-                            MessageLogger.Write(prefix, line);
-                    }
-                   
                 }
+
+                ConsoleColor.SetColor(textColor);
+                foreach (string line in error.ReadToEnd().Split(Environment.NewLine.ToCharArray()))
+                {
+                    if (line.Trim().Length > 0)
+                        MessageLogger.Write(prefix, line);
+                }
+            }
 
             ConsoleColor.SetColor(ConsoleColor.BuildColor.Default);
         }
 
         public void Execute()
         {
-           Execute("exec");
+            Execute("exec");
         }
     }
 }
