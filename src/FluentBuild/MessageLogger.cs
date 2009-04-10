fd931554d08771c933e41ac51d634a667a1237bc
dave@solidhouse.com <dave@solidhouse.com@b5b6f76c-248a-11de-9353-0b00b05e739f>
2009-04-10 21:28:31 +0000

    -Refactoring color support to add indentation and seperate the concerns a bit

--- src/FluentBuild/MessageLogger.cs
+++ src/FluentBuild/MessageLogger.cs
@@ -6,7 +6,26 @@ namespace FluentBuild
     {
         public static void Write(string type, string message)
         {
-            Console.WriteLine(String.Format("   [{0}] {1}",type, message));
+            string outputMessage = String.Format("[{0}] {1}", type, message);
+            do
+            {
+                int length = 0;
+                if (outputMessage.Length > Console.WindowWidth - 20)
+                    length = Console.WindowWidth - 20;
+                else
+                    length = outputMessage.Length;
+
+                //ugly indentation code. Better way to do it but brain hurts
+                if (!outputMessage.Substring(0, length).Contains(String.Format("[{0}]", type)))
+                {
+                    Console.WriteLine("\t".PadRight(type.Length+4, ' ') + outputMessage.Substring(0, length));
+                }
+                else
+                {
+                    Console.WriteLine("\t" + outputMessage.Substring(0, length));
+                }
+                outputMessage = outputMessage.Substring(length);
+            } while (outputMessage.Trim().Length > 0);
         }
     }
 }
