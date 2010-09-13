59cb9d4844ce6879250459dc2084a2a342eb69a3
dave@solidhouse.com <dave@solidhouse.com@b5b6f76c-248a-11de-9353-0b00b05e739f>
2010-09-13 17:06:30 +0000

    -Dropped idea of marker interface on build file. -Added base class of BuildFile so that method calls are handled by the runner now so that better output can be achieved.

---
+++ trunk/src/FluentBuild/FilesAndDirectories/RenameBuildArtifact.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+
+namespace FluentBuild
+{
+    public class RenameBuildArtifact
+    {
+        private readonly BuildArtifact _buildArtifact;
+        private readonly IFileSystemWrapper _fileSystemWrapper;
+
+        public RenameBuildArtifact(IFileSystemWrapper fileSystemWrapper, BuildArtifact buildArtifact)
+        {
+            _fileSystemWrapper = fileSystemWrapper;
+            _buildArtifact = buildArtifact;
+        }
+
+        public RenameBuildArtifact(BuildArtifact buildArtifact) : this(new FileSystemWrapper(), buildArtifact)
+        {
+        }
+
+        public BuildArtifact To(string newName)
+        {
+            //TODO: may have to determine folder of original build artifact for this to work
+            
+            _fileSystemWrapper.MoveFile(_buildArtifact.ToString(), Path.GetDirectoryName(_buildArtifact.ToString()) + "\\" + newName);
+            return _buildArtifact;
+        }
+    }
+}
--- trunk/src/FluentBuild/IBuild.cs
+++ trunk/src/FluentBuild/IBuild.cs
@@ -1,7 +1,36 @@
-namespace FluentBuild.BuildFile
+using System;
+using System.Collections.Generic;
+
+namespace FluentBuild
 {
     public interface IBuild
     {
         void Execute();
+    }
+
+    public class BuildFile
+    {
+        private Queue<Action> tasks;
+
+        public void InvokeNextTask()
+        {
+            if (tasks.Count == 0)
+                return;
+            Action dequeue = tasks.Dequeue();
+            MessageLogger.WriteHeader(dequeue.Method.Name);
+            dequeue.Invoke();
+            InvokeNextTask();
+        }
+
+        public BuildFile()
+        {
+            this.tasks = new Queue<Action>();
+        }
+
+        public void AddTask(Action task)
+        {
+            tasks.Enqueue(task);
+        }
+
     }
 }
