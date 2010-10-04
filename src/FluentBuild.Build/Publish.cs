75678580db64349f12dba2fceeff3d5d2317d820
dave@solidhouse.com <dave@solidhouse.com@b5b6f76c-248a-11de-9353-0b00b05e739f>
2010-10-04 01:11:28 +0000

    -Adding in PublishNightly task

--- src/FluentBuild.Build/Publish.cs
+++ src/FluentBuild.Build/Publish.cs
@@ -3,20 +3,11 @@ using FluentBuild.Publishing;
 
 namespace Build
 {
-    public class PublishNightly : Publish
-    {
-        public PublishNightly()
-        {
-            _version = Properties.CommandLineProperties.GetProperty("Version");
-            _finalFileName = "FluentBuild-Nightly-" + _version + ".zip";
-        }
-    }
-
     public class Publish : Default
     {
         internal readonly BuildArtifact AssemblyFluentBuildRelease;
         internal readonly BuildArtifact AssemblyFluentBuildRunnerRelease;
-        internal readonly string _finalFileName;
+        internal string _finalFileName;
         internal BuildArtifact ZipFilePath;
 
         public Publish()
