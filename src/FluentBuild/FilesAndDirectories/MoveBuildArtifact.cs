0b5f9c5f8d86df836b7e809d94ccf55a8cfe8d38
dave@solidhouse.com <dave@solidhouse.com@b5b6f76c-248a-11de-9353-0b00b05e739f>
2010-10-19 15:09:47 +0000

    -Added buildArtifact.Move

---
+++ src/FluentBuild/FilesAndDirectories/MoveBuildArtifact.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+using FluentBuild.Core;
+using FluentBuild.Utilities;
+
+namespace FluentBuild.FilesAndDirectories
+{
+    ///<summary>
+    /// Moves a build artifact to a destination
+    ///</summary>
+    public class MoveBuildArtifact : Failable<MoveBuildArtifact>
+    {
+        private readonly BuildArtifact _buildArtifact;
+        private readonly IFileSystemWrapper _fileSystemWrapper;
+
+
+        internal MoveBuildArtifact(IFileSystemWrapper fileSystemWrapper, BuildArtifact buildArtifact)
+        {
+            _fileSystemWrapper = fileSystemWrapper;
+            _buildArtifact = buildArtifact;
+        }
+
+        internal MoveBuildArtifact(BuildArtifact buildArtifact)
+            : this(new FileSystemWrapper(), buildArtifact)
+        {
+        }
+
+        ///<summary>
+        /// Moves a file to a destination
+        ///</summary>
+        ///<param name="destination">the new location of the file</param>
+        public BuildArtifact To(string destination)
+        {
+            if (_fileSystemWrapper.DirectoryExists(destination))
+            {
+                destination = Path.Combine(destination, _buildArtifact.FileName());
+            }
+
+            FailableActionExecutor.DoAction(OnError, _fileSystemWrapper.MoveFile, _buildArtifact.ToString(), destination);
+            _buildArtifact.Path = destination;
+            return _buildArtifact;
+        }
+
+        ///<summary>
+        /// Moves a file to a destination
+        ///</summary>
+        ///<param name="destination">the new location of the file</param>
+        public BuildArtifact To(BuildArtifact destination)
+        {
+            return To(destination.ToString());
+        }
+
+        ///<summary>
+        /// Moves a file to a destination
+        ///</summary>
+        ///<param name="destination">the new location of the file</param>
+        public BuildArtifact To(BuildFolder destination)
+        {
+            return To(destination.ToString());
+        }
+    }
+}
