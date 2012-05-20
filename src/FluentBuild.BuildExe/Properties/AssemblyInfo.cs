e88ebcf6ca39c3a5525073131ccc26828ccbde72
dave@solidhouse.com <dave@solidhouse.com@b5b6f76c-248a-11de-9353-0b00b05e739f>
2012-05-20 00:26:11 +0000

    -v1.1.0.0

--- src/FluentBuild.BuildExe/Properties/AssemblyInfo.cs
+++ src/FluentBuild.BuildExe/Properties/AssemblyInfo.cs
@@ -7,4 +7,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyrightAttribute("Copyright 2009-2012")]
 [assembly: AssemblyDescriptionAttribute("FluentBuild Build Runner")]
 [assembly: AssemblyProductAttribute("FluentBuild")]
-[assembly: AssemblyVersionAttribute("1.0.1.0")]
+[assembly: AssemblyVersionAttribute("1.1.0.0")]
