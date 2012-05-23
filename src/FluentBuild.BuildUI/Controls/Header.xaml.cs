e07b280d9b935f073cd7c3229f5d777c1537f445
dave@solidhouse.com <dave@solidhouse.com@b5b6f76c-248a-11de-9353-0b00b05e739f>
2012-05-23 15:47:43 +0000

    -FluentBuild.UI work

---
+++ src/FluentBuild.BuildUI/Controls/Header.xaml.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using UserControl = System.Windows.Controls.UserControl;
+
+namespace FluentBuild.BuildUI.Controls
+{
+    /// <summary>
+    /// Interaction logic for Header.xaml
+    /// </summary>
+    public partial class Header : UserControl, INotifyPropertyChanged
+    {
+        private string _buildAssembly;
+
+        public Header()
+        {
+            InitializeComponent();
+            Path.Text = SettingHelper.LastPath;
+            WorkingDirectory.Text = SettingHelper.LastWorkingDirectory;
+            Compile(Path.Text);
+        }
+
+                
+        public event EventHandler Reset;
+
+        public void InvokeReset()
+        {
+            EventHandler handler = Reset;
+            if (handler != null) handler(this, new EventArgs());
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        private void BrowseClick(object sender, RoutedEventArgs e)
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                dialog.SelectedPath = Path.Text;
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    Path.Text = dialog.SelectedPath;
+                    WorkingDirectory.Text = dialog.SelectedPath;
+                    InvokePropertyChanged("Path");
+                    InvokePropertyChanged("WorkingDirectory");
+                    Compile(dialog.SelectedPath);
+                }
+            }
+        }
+
+        private void Compile(string selectedPath)
+        {
+            if (string.IsNullOrEmpty(selectedPath))
+                return;
+
+            InvokeReset();
+
+            Defaults.Logger.Verbosity = VerbosityLevel.Full;
+            Defaults.Logger.WriteHeader("Compile Build File");
+            if (!Directory.Exists(selectedPath))
+            {
+                Defaults.Logger.WriteError("Folder Not Found", "Could not find the build folder at " + selectedPath);
+                return;
+            }
+
+            try
+            {
+                _buildAssembly = Compiler.BuildAssemblyFromSources(selectedPath);
+                Targets.ItemsSource = Compiler.FindBuildClasses(_buildAssembly);
+                Targets.SelectedIndex = 0;
+                InvokePropertyChanged("Targets");
+            }
+            catch (Exception e)
+            {
+                Defaults.Logger.WriteError("Compile Build File", e.ToString());
+            }
+
+            Defaults.Logger.WriteHeader("Done");
+        }
+
+        public void InvokePropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(name));
+        }
+
+        private void RunCurrentBuild(object sender, RoutedEventArgs e)
+        {
+            SettingHelper.LastPath = Path.Text;
+            SettingHelper.LastWorkingDirectory = WorkingDirectory.Text;
+
+            InvokeReset();
+            
+            Directory.SetCurrentDirectory(WorkingDirectory.Text);
+            Environment.CurrentDirectory = WorkingDirectory.Text;
+
+            var runner = new Runner(Targets.SelectedItem.ToString(), _buildAssembly);
+            runner.Run();
+        }
+
+        private void Refresh(object sender, RoutedEventArgs e)
+        {
+            Compile(Path.Text);
+        }
+
+        private void WorkingDirectoryBrowseClick(object sender, RoutedEventArgs e)
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                dialog.SelectedPath = WorkingDirectory.Text;
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    WorkingDirectory.Text = dialog.SelectedPath;
+                    InvokePropertyChanged("WorkingDirectory");
+                }
+            }
+        }
+
+    }
+}
--- src/FluentBuild.BuildUI/MainWindow.xaml.cs
+++ src/FluentBuild.BuildUI/MainWindow.xaml.cs
@@ -6,7 +6,7 @@ using System.Windows.Forms;
 
 namespace FluentBuild.BuildUI
 {
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : Window
     {
         private string _buildAssembly;
 
@@ -14,84 +14,12 @@ namespace FluentBuild.BuildUI
         {
             InitializeComponent();
             Defaults.SetLogger(BuildProgress);
-
-            Path.Text = SettingHelper.LastPath;
-            WorkingDirectory.Text = SettingHelper.LastWorkingDirectory;
-            Compile(Path.Text);
+                
         }
 
-        #region INotifyPropertyChanged Members
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        #endregion
-
-        private void BrowseClick(object sender, RoutedEventArgs e)
+        private void ResetOutput(object sender, EventArgs e)
         {
-            using (var dialog = new FolderBrowserDialog())
-            {
-                dialog.SelectedPath = Path.Text;
-                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-                {
-                    Path.Text = dialog.SelectedPath;
-                    InvokePropertyChanged("Path");
-                    Compile(dialog.SelectedPath);
-                    
-                }
-            }
-        }
-
-        private void Compile(string selectedPath)
-        {
-            if (string.IsNullOrEmpty(selectedPath))
-                return;
-
             BuildProgress.Reset();
-            Defaults.Logger.Verbosity = VerbosityLevel.Full;
-            Defaults.Logger.WriteHeader("Compile Build File");
-            if (!Directory.Exists(selectedPath))
-            {
-                Defaults.Logger.WriteError("Folder Not Found", "Could not find the build folder at " + selectedPath);
-                return;
-            }
-
-            try
-            {
-                _buildAssembly = Compiler.BuildAssemblyFromSources(selectedPath);
-                Targets.ItemsSource = Compiler.FindBuildClasses(_buildAssembly);
-                Targets.SelectedIndex = 0;
-                InvokePropertyChanged("Targets");
-            }
-            catch (Exception e)
-            {
-                Defaults.Logger.WriteError("Compile Build File", e.ToString());
-            }
-
-            Defaults.Logger.WriteHeader("Done");
-        }
-
-        public void InvokePropertyChanged(string name)
-        {
-            PropertyChangedEventHandler handler = PropertyChanged;
-            if (handler != null) handler(this, new PropertyChangedEventArgs(name));
-        }
-
-        private void RunCurrentBuild(object sender, RoutedEventArgs e)
-        {
-            SettingHelper.LastPath = Path.Text;
-            SettingHelper.LastWorkingDirectory = WorkingDirectory.Text;
-
-            BuildProgress.Reset();
-            Directory.SetCurrentDirectory(WorkingDirectory.Text);
-            Environment.CurrentDirectory = WorkingDirectory.Text;
-
-            var runner = new Runner(Targets.SelectedItem.ToString(), BuildProgress, _buildAssembly);
-            runner.Run();
-        }
-
-        private void Refresh(object sender, RoutedEventArgs e)
-        {
-            Compile(Path.Text);
         }
     }
 }
